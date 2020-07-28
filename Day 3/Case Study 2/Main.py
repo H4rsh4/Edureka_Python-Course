1354584263118a50362372b260c715877d78bbc7
H.A <36944939+H4rsh4@users.noreply.github.com>
2020-07-27 20:58:51 -0400

    Day 3/ CS2 &CS3

--- Day 3/Case Study 2/Main.py
+++ Day 3/Case Study 2/Main.py
@@ -1,4 +1,4 @@
-#[UNCLEAR OBJECTIVE]
+#No fields of data of customers who are ineligible for loan in the data source
 
 
 import pandas as pd
@@ -6,17 +6,20 @@ import pandas as pd
 bank_data = 'E:\Edureka_Python-Course\Day 3\Case Study 2\\bank-data.csv'
 df_raw = pd.read_csv(bank_data, low_memory=False)
 
-jobs = {j for j in df_raw.job}
+jobs = {str(j).lower() for j in df_raw.job}
 print(jobs)
 
-eligibility = {job:(True if elig) for }
+eligible_jobs = []
 #print(jobs)
 
-''''''
+max_age = max(df_raw.age)
+min_age = min(df_raw.age)
+
+
 print(df_raw.head())
 
 def check_eligibility(profession:str):
-    return True if profession in eligible_jobs else False
+    return True if profession.lower() in eligible_jobs else False
 
 while True:
     print('If you want to exit, type "exit" in the profession')
@@ -24,3 +27,5 @@ while True:
     
     if check_eligibility(profession):
         print('Client is eligible')
+    else:
+        print("Not eligible")
